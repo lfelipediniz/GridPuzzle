2f1826c1487955be0712fcdcdbad8a456988b665
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2024-05-22 13:17:40 -0300

    fix: update main

--- BoardGUI.java
+++ BoardGUI.java
@@ -81,20 +81,4 @@ public class BoardGUI extends JFrame {
             }
         }
     }
-
-    public static void main(String[] args) {
-        // ask the user for the size of the board
-        String sizeInput = JOptionPane.showInputDialog("Enter the size of the board (n for an n x n board):");
-        int size = Integer.parseInt(sizeInput);
-
-        // ask the user for the initial state of the board
-        int[] initialState = new int[size * size];
-        for (int i = 0; i < size * size; i++) {
-            String valueInput = JOptionPane.showInputDialog("Enter the value for position " + i + ":");
-            initialState[i] = Integer.parseInt(valueInput);
-        }
-
-        Board board = new Board(initialState);
-        new BoardGUI(board);
-    }
 }
--- Main.java
+++ Main.java
@@ -1,32 +1,19 @@
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        // ask the user for the size of the board
+        String sizeInput = JOptionPane.showInputDialog("Enter the size of the board (n for an n x n board):");
+        int size = Integer.parseInt(sizeInput);
 
-        // read initial state and moves
-        String[] input = scanner.nextLine().split(" ");
-
-        int[] initialState = new int[input.length];
-
-        // convert input to int array
-        for (int i = 0; i < input.length; i++) {
-            initialState[i] = Integer.parseInt(input[i]);
+        // ask the user for the initial state of the board
+        int[] initialState = new int[size * size];
+        for (int i = 0; i < size * size; i++) {
+            String valueInput = JOptionPane.showInputDialog("Enter the value for position " + i + ":");
+            initialState[i] = Integer.parseInt(valueInput);
         }
 
         Board board = new Board(initialState);
-        String moves = scanner.nextLine();
-        scanner.close();
-
-        // print initial position
-        board.printBoard();
-
-        for (char move : moves.toCharArray()) {
-            board.move(move);
-            // print position after each move
-            board.printBoard();
-        }
-
-        System.out.println("Posicao final: " + board.verifyPosition());
+        new BoardGUI(board);
     }
 }
